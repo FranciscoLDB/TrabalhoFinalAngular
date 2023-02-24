240a1d2da7cd040d1d7f02f26db45b3127d4c43d
franciscoldb <fluis905@gmail.com>
2023-02-24 01:47:48 -0300

    ajustes estilos

--- src/app/pagina-pessoal/pagina-pessoal.component.ts
+++ src/app/pagina-pessoal/pagina-pessoal.component.ts
@@ -7,19 +7,23 @@ import { UsuariosService } from '../services/usuarios.service';
   styleUrls: ['./pagina-pessoal.component.css']
 })
 export class PaginaPessoalComponent {
-  nome = `Leandro`;
-  idade = 20;
-  peso = `77 kg`;
-  altura = `195 cm`;
   pessoa = {
-    nome: `Leandro`,
+    nome: 'Leandro',
     idade: 20,
-    peso: `77 kg`,
-    altura: `195 cm`,
-    metas: [    ],
+    peso: '77 kg',
+    altura: '195 cm',
+    sexo: 'Masculino',
+    objetivo: 'Ganhar massa',
+    metas: [
+      { desc: 'Calorias Diarias', cor: '#63B83F', progresso: 600, meta: 2000},
+      { desc: 'Calorias Semanais', cor: '#63B83F', progresso: 6000, meta: 14000},
+      { desc: 'Água Diaria', cor: '#38B6FF', progresso: 1750, meta: 2000},
+      { desc: 'Água Semanal', cor: '#38B6FF', progresso: 8500, meta: 14000},
+      { desc: 'Excercicios Semanais', cor: '#FFDE59', progresso: 4, meta: 7},
+      { desc: 'Excercicios Mensais', cor: '#FFDE59', progresso: 12, meta: 25},
+    ],
   }
 
   constructor(private service: UsuariosService){
-
   }
 }
